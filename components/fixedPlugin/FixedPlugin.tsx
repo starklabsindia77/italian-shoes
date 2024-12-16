a4f29878b154dcd96825bedd8f06e8ac1801af89
varungirikon <155966687+varungirikon@users.noreply.github.com>
2024-12-17 01:06:02 +0530

    initial commit

---
+++ components/fixedPlugin/FixedPlugin.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useThemeContext } from "@/providers/ThemeProvider";
+import { RiMoonFill, RiSunFill } from "react-icons/ri";
+
+export default function FixedPlugin() {
+  const { theme, setTheme } = useThemeContext()
+
+  return (
+    <button
+      className="border-px fixed bottom-[30px] right-[35px] !z-[99] flex h-[60px] w-[60px] items-center justify-center rounded-full border-[#6a53ff] bg-gradient-to-br from-brandLinear to-blueSecondary p-0"
+      onClick={() => {
+        theme === 'dark' ? setTheme('dark') : setTheme('dark')
+      }}
+    >
+      <div className="cursor-pointer text-gray-600">
+        {theme === 'dark' ? (
+          <RiSunFill className="h-4 w-4 text-white" />
+        ) : (
+          <RiMoonFill className="h-4 w-4 text-white" />
+        )}
+      </div>
+    </button>
+  );
+}

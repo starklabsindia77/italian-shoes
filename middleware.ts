8442aff0c7cdf0116e54fdc820bed3e89e180080
Varun Pratap Singh <59950166+starklabsindia77@users.noreply.github.com>
2026-03-30 15:12:47 +0530

    Add empty lines for better code readability

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
+
+
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
